58b56cd481e2892a51ef8a5cd4adb9a2644f2f66
NatalliaMetsko <natallia.metsko@gmail.com>
2017-11-12 00:08:42 +0300

    Optimized imports in OrderDaoImpl.

--- inventory/src/main/java/com/netcracker/metsko/dao/implementation/OrderDaoImpl.java
+++ inventory/src/main/java/com/netcracker/metsko/dao/implementation/OrderDaoImpl.java
@@ -3,7 +3,7 @@ package com.netcracker.metsko.dao.implementation;
 import com.netcracker.metsko.dao.OrderDao;
 import com.netcracker.metsko.entity.Order;
 import com.netcracker.metsko.entity.OrderItem;
-import com.sun.xml.internal.bind.v2.model.core.ID;
+
 
 import java.sql.SQLException;
 import java.util.List;
