e1e5c2265f01bd830d401d87a949428278874d02
NatalliaMetsko <natallia.metsko@gmail.com>
2017-11-12 00:07:14 +0300

    Optimized imports. Added SQLException in OrderDAoImpl.java .

--- catalog/src/main/java/com/netcracker/metsko/dao/implementation/OfferDaoImpl.java
+++ catalog/src/main/java/com/netcracker/metsko/dao/implementation/OfferDaoImpl.java
@@ -5,7 +5,6 @@ import com.netcracker.metsko.entity.Category;
 import com.netcracker.metsko.entity.Offer;
 import com.netcracker.metsko.entity.Price;
 import com.netcracker.metsko.entity.Tag;
-import com.sun.xml.internal.bind.v2.model.core.ID;
 
 import java.sql.SQLException;
 import java.util.List;
