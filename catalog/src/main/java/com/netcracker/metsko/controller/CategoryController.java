2014e44374de3f58f8a6aa0ebb853bd53874ef3a
NatalliaMetsko <natallia.metsko@gmail.com>
2017-12-05 14:50:50 +0300

    Added @ApiOperation in CategoryController.

--- catalog/src/main/java/com/netcracker/metsko/controller/CategoryController.java
+++ catalog/src/main/java/com/netcracker/metsko/controller/CategoryController.java
@@ -7,6 +7,9 @@ import com.netcracker.metsko.exceptions.NotDeletedException;
 import com.netcracker.metsko.exceptions.NotFoundException;
 import com.netcracker.metsko.exceptions.NotUpdatedException;
 import com.netcracker.metsko.service.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/catalog/categories")
-public class CategoryController {
+public class    CategoryController {
 
     @Autowired
     private CategoryService categoryService;
@@ -27,12 +30,23 @@ public class CategoryController {
     }
 
     @PostMapping
+    @ApiOperation(httpMethod = "POST",
+            value = "Create a category",
+            response = Category.class,
+            nickname="createCategory")
+//  @ApiResponse(code = 500, message = "Category not created")
     public ResponseEntity<Category> createCategory(@RequestBody Category newCategory) throws NotCreatedException, SQLException {
         categoryService.createCategory(newCategory);
         return  new ResponseEntity<Category>(newCategory, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
+    @ApiOperation(httpMethod = "GET",
+            value = "Find a category by id",
+            response = Category.class,
+            nickname="findById")
+//    @ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid ID supplied"),
+//            @ApiResponse(code = 404, message = "Category not found") })
     public ResponseEntity<Category> findById(@PathVariable("id") Long categoryId) throws NotFoundException, SQLException {
         Category category = categoryService.findById(categoryId);
         return new ResponseEntity<Category>(category, HttpStatus.FOUND);
@@ -40,30 +54,52 @@ public class CategoryController {
     }
 
     @GetMapping(value = "/name/{name}")
+    @ApiOperation(httpMethod = "GET",
+            value = "Find a category by it's name",
+            response = Category.class,
+            nickname="findByName")
     public ResponseEntity<Category> findByName(@PathVariable("name") String name) throws NotFoundException, SQLException {
         Category category = categoryService.findByName(name);
         return new ResponseEntity<Category>(category, HttpStatus.FOUND);
     }
 
     @GetMapping
+    @ApiOperation(httpMethod = "GET",
+            value = "Find all categories",
+            response = Category.class,
+            nickname="findAll",
+            responseContainer = "List")
     public ResponseEntity<List<Category>> findAll() throws NotFoundException, SQLException {
         List<Category> categoryList= categoryService.findAll();
         return new ResponseEntity<List<Category>>(categoryList, HttpStatus.FOUND);
     }
 
     @PutMapping
+    @ApiOperation(httpMethod = "PUT",
+            value = "Update category",
+            response = Category.class,
+            nickname="updateCategory")
     public ResponseEntity<Category> updateCategory(@RequestBody Category category) throws NotUpdatedException, SQLException {
         Category updatedCategory = categoryService.updateCategory(category);
         return new ResponseEntity<Category>(updatedCategory, HttpStatus.OK);
     }
 
     @DeleteMapping( value = "/{id}")
+    @ApiOperation(httpMethod = "DELETE",
+            value = "Delete a category by id",
+            response = Long.class,
+            nickname="deleteCategory")
     public ResponseEntity<Long> deleteCategory(@PathVariable("id") Long categoryId) throws NotDeletedException, SQLException {
         categoryService.deleteCategory(categoryId);
         return  new ResponseEntity<Long>(categoryId, HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}/offers")
+    @ApiOperation(httpMethod = "GET",
+            value = "Find offers belonged to the category (by id)",
+            response = Offer.class,
+            nickname="findOfferList",
+            responseContainer = "List")
     public ResponseEntity<List<Offer>> findOfferList(@PathVariable("id") Long id) throws NotFoundException, SQLException {
         List<Offer> offerList= categoryService.findOfferList(id);
         return new ResponseEntity<List<Offer>>(offerList, HttpStatus.FOUND);
